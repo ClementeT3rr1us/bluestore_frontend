61992254305b492f4a4bef3eda6c2c7c0828e799
clemente <clementedino681@gmail.com>
2025-03-18 23:09:31 -0300

    starting the stock page

--- src/components/Stock/Stock.jsx
+++ src/components/Stock/Stock.jsx
@@ -13,6 +13,81 @@ export default function Stock(params) {
             <tr>
                 <td>nome</td><td>descrição</td><td>valor</td><td>quantidade</td>
             </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
+            <tr>
+                <td>roupa</td><td>detalhe</td><td>R$ 50.38</td><td>105</td>
+            </tr>
         </table>
       </div>
     </>
